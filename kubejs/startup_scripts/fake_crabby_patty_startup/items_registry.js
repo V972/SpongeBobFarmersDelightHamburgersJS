06efef9d7973d238cc100655f901f640ae4851a6
AdeptusAutumn <mrcreeperag@gmail.com>
2024-04-21 12:37:49 +0200

    Update items_registry.js
    Updated items_registry.js to KubeJS6

--- kubejs/startup_scripts/fake_crabby_patty_startup/items_registry.js
+++ kubejs/startup_scripts/fake_crabby_patty_startup/items_registry.js
@@ -1,15 +1,20 @@
 // Fake FD Hamburger
-// Startup part
+// Funni part
 // 
 // Reference to mass-produced Crabby Patty
 // 
-// MC 1.18
+// MC 1.20.1
 // Forge
 // Mods: Farmer's Delight, Create, Create: Dreams & Desires
 //
 // https://www.youtube.com/watch?v=fBB2mD7w3gM
+//
+//Updated by AdeptusAutumn
 
-onEvent('item.registry', event => {
+//Old 1.18 Code
+//onEvent('item.registry', event => {
+
+StartupEvents.registry('item', event => {
     event.create('hamburger_shaped_clay', 'create:sequenced_assembly')
         .displayName('Hamburger-shaped Clay')
         .texture('kubejs:item/fake_crabby_patty/hamburger_shaped_clay');
